11ac249090a5e4fe95111a9e7609210c14c2a54a
Josh Wolfe <thejoshwolfe@gmail.com>
2014-09-26 16:01:45 -0700

    use whynoteq to check deep equality

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var curlydiff = require("../");
+var assertDeepEqual = require('whynoteq').assertDeepEqual;
 
 test({a:1, b:2}, {a:1});
 test({a:1, b:2}, {a:1, b:3});
@@ -9,15 +10,12 @@ function test(o1, o2) {
   testOneWay(o2, o1);
   function testOneWay(o1, o2) {
     var patch = curlydiff.diff(o1, o2);
-    var patched = curlydiff.apply(o1, patch);
-    if (equals(o2, patched)) return process.stdout.write("+");
-    console.log("");
-    throw new Error("expected " + JSON.stringify(o2) + " == " + JSON.stringify(patched));
+    var patched = curlydiff.apply(deepCopy(o1), patch);
+    assertDeepEqual(o2, patched);
+    process.stdout.write("+");
   }
 }
 
-function equals(o1, o2) {
-  if (o1 === o2) return true;
-  // TODO: implement
-  return false;
+function deepCopy(object) {
+  return JSON.parse(JSON.stringify(object));
 }
