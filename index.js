d9080e811db81377a83dbb5c5883516d12f98fc1
Josh Wolfe <thejoshwolfe@gmail.com>
2014-09-26 16:44:45 -0700

    implementing diff and apply. adding tests

--- index.js
+++ index.js
@@ -2,7 +2,50 @@ module.exports.diff = diff;
 module.exports.apply = apply;
 
 function diff(from, to) {
+  if (!isObject(from) || !isObject(to)) return cannonicalize(to);
+  // both are objects
+  var result = {};
+  Object.keys(from).forEach(function(key) {
+    var fromChild = from[key];
+    var toChild = to[key];
+    if (fromChild === toChild) return;
+    var childDiff = diff(fromChild, toChild);
+    if (isObject(childDiff) && Object.keys(childDiff).length === 0) return;
+    // there's a difference
+    result[key] = childDiff;
+  });
+  Object.keys(to).forEach(function(key) {
+    if (key in from) return; // handled above
+    result[key] = to[key];
+  });
+  return result;
 }
 
 function apply(object, patch) {
+  if (!isObject(object) || !isObject(patch)) return patch;
+  // both are objects
+  Object.keys(patch).forEach(function(key) {
+    var patchChild = patch[key];
+    if (patchChild == null) {
+      // removed
+      delete object[key];
+      return;
+    }
+    // one or both of this assignment and this function call will have side effects
+    object[key] = apply(object[key], patchChild);
+  });
+  return object;
+}
+
+function isObject(object) {
+  if (object == null) return false;
+  if (typeof object !== "object") return false;
+  if (object.constructor === Array) return false;
+  return true;
+}
+
+function cannonicalize(value) {
+  // convert undefined to null
+  if (value == null) return null;
+  return value;
 }
--- test/test.js
+++ test/test.js
@@ -1,19 +1,35 @@
 var curlydiff = require("../");
 var assertDeepEqual = require('whynoteq').assertDeepEqual;
 
+console.log("\nprimitives:");
+test(1, 1);
+test(1, null);
+test(null, 1);
+test(1, 2);
+test(1, true);
+test(1, "string");
+
+console.log("\narrays are primitives:");
+test(["a", "b"], ["a", "b", "c"]);
+
+console.log("\nsimple objects:");
+test({a:1, b:2}, {a:1, b:2});
 test({a:1, b:2}, {a:1});
+test({a:1, b:2}, {a:1, b:2, c:3});
 test({a:1, b:2}, {a:1, b:3});
 
-console.log("");
+console.log("\nnested objects:");
+test({a:1, b:{c:2, d:3}}, {a:1, b:{c:2, d:3}});
+test({a:1, b:{c:2, d:3}}, {a:1, b:{c:2}});
+test({a:1, b:{c:2, d:3}}, {a:1, b:{c:2, d:3, e:4}});
+test({a:1, b:{c:2, d:3}}, {a:1, b:{c:2, d:4}});
+test({a:1, b:{c:2, d:3}}, {a:2, b:{c:2, d:3}});
+
 function test(o1, o2) {
-  testOneWay(o1, o2);
-  testOneWay(o2, o1);
-  function testOneWay(o1, o2) {
-    var patch = curlydiff.diff(o1, o2);
-    var patched = curlydiff.apply(deepCopy(o1), patch);
-    assertDeepEqual(o2, patched);
-    process.stdout.write("+");
-  }
+  var patch = curlydiff.diff(o1, o2);
+  var patched = curlydiff.apply(deepCopy(o1), patch);
+  assertDeepEqual(o2, patched);
+  console.log("pass: " + JSON.stringify(patch));
 }
 
 function deepCopy(object) {
